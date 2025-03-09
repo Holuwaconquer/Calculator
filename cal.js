aec76eed7167051d61bcfddb530866efd98ef629
Okikiola Fakorede <holuwaconquer488@gmail.com>
2025-03-09 11:04:26 +0100

    Created a calculator web app with HTML, CSS and JAVASCRIPT

---
+++ cal.js
@@ -0,0 +1,45 @@
+const display = document.getElementById('display')
+        const sumdisplay = document.getElementById('displayy')
+        const sunBtn = document.getElementById('sunBtn')
+        const moonBtn = document.getElementById('moonBtn')
+        const calContainer = document.querySelector('.calContainer')
+        sunBtn.addEventListener('click', ()=> {
+            calContainer.style.backgroundColor = 'white'
+            display.style.backgroundColor = 'white'
+            display.style.color = 'black'
+            sumdisplay.style.backgroundColor = 'white'
+            sumdisplay.style.color = 'black'
+            document.getElementById('key').style.backgroundColor = "#f8f9fa"
+            // document.getElementsByTagName('button').style.backgroundColor = "green"
+
+        })
+        moonBtn.addEventListener('click', ()=>{
+            calContainer.style.backgroundColor = ''
+            display.style.backgroundColor = ''
+            display.style.color = ''
+            sumdisplay.style.backgroundColor = ''
+            sumdisplay.style.color = ''
+            document.getElementById('key').style.backgroundColor = ""
+        })
+        const appendToDisplay = (val) =>{
+            display.value += val;
+        }
+        const clearfunc = ()=>{
+            display.value = "";
+            sumdisplay.value = "";
+        }
+        const clearfun = ()=>{
+            display.value = "";
+        }
+        const calculate =()=>{
+            try{
+                sumdisplay.value = eval(display.value)
+            }
+            catch{
+                sumdisplay.value = "Error"
+            }
+        }
+        
+        const deleteLast = () =>{
+            display.value = display.value.slice(0, -1);
+        }
