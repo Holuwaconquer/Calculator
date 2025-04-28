25633512c9eed29ef19afe4968114e5531ba26f1
Okikiola Fakorede <holuwaconquer488@gmail.com>
2025-04-28 15:04:23 +0100

    updated with PWA features

--- cal.js
+++ cal.js
@@ -47,3 +47,14 @@ const display = document.getElementById('display')
         const deleteLast = () =>{
             display.value = display.value.slice(0, -1);
         }
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('./service-worker.js').then(function(registration) {
+        console.log('Service Worker registered with scope:', registration.scope);
+        }, function(err) {
+        console.log('Service Worker registration failed:', err);
+        });
+    });
+    }
+          
